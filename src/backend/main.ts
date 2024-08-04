336d3a487bd3a77db22f557339147d0526ef5b29
le-xot <lexot.dev@gmail.com>
2024-08-05 02:46:42 +0500

    feat: add Joi

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { enviroment } from 'src/utils/enviroments';
+import env from 'src/utils/enviroments';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +12,6 @@ async function bootstrap() {
     customCss: `.swagger-ui .topbar { display: none } .version { display: none } .swagger-ui .info .title small.version-stamp { display: none } .swagger-ui .info .title small { display: none }`,
   });
 
-  await app.listen(enviroment.appPort);
+  await app.listen(env.appPort);
 }
 bootstrap();
--- src/utils/enviroments.ts
+++ src/utils/enviroments.ts
@@ -1,27 +1,41 @@
-import 'dotenv/config';
+import Joi from 'joi';
+import dotenv from 'dotenv';
+import path from 'path';
 
-export interface Enviroment {
-  dataSourseUrl: string;
+// Загружаем переменные из .env файла
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+// Определяем схему валидации с использованием Joi
+const schema = Joi.object({
+  DATASOURCE_URL: Joi.string().uri().required().label('DATASOURCE_URL'),
+  JWT_SECRET: Joi.string().required().label('JWT_SECRET'),
+  APP_PORT: Joi.number()
+    .integer()
+    .min(1)
+    .max(65535)
+    .required()
+    .label('APP_PORT'),
+}).unknown(true); // unknown(true) позволяет включать необязательные переменные, которые не были явно указаны в схеме
+
+// Валидируем переменные окружения
+const { error, value: envVars } = schema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+// Определяем интерфейс для конфигурации
+interface Config {
+  datasourceUrl: string;
   jwtSecret: string;
   appPort: number;
 }
 
-export function validated(): Enviroment {
-  const { DATASOURSE_URL, JWT_SECRET, APP_PORT } = process.env;
+// Экспортируем валидированные переменные окружения
+const env: Config = {
+  datasourceUrl: envVars.DATASOURCE_URL,
+  jwtSecret: envVars.JWT_SECRET,
+  appPort: envVars.APP_PORT,
+};
 
-  if (!DATASOURSE_URL || !JWT_SECRET || !APP_PORT) {
-    throw new Error('Enviroments not found');
-  }
-
-  const dataSourseUrl = DATASOURSE_URL;
-  const jwtSecret = JWT_SECRET;
-  const appport = Number(APP_PORT);
-
-  return {
-    dataSourseUrl,
-    jwtSecret,
-    appPort: appport,
-  };
-}
-
-export const enviroment = validated();
+export default env;
