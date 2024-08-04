59279b6da6d02fa3dae30b20d165a5662db55244
le-xot <lexot.dev@gmail.com>
2024-08-05 02:52:54 +0500

    chore: remove comments

--- src/utils/enviroments.ts
+++ src/utils/enviroments.ts
@@ -1,11 +1,8 @@
 import Joi from 'joi';
 import dotenv from 'dotenv';
-import path from 'path';
 
-// Загружаем переменные из .env файла
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config();
 
-// Определяем схему валидации с использованием Joi
 const schema = Joi.object({
   DATASOURCE_URL: Joi.string().uri().required().label('DATASOURCE_URL'),
   JWT_SECRET: Joi.string().required().label('JWT_SECRET'),
@@ -15,23 +12,20 @@ const schema = Joi.object({
     .max(65535)
     .required()
     .label('APP_PORT'),
-}).unknown(true); // unknown(true) позволяет включать необязательные переменные, которые не были явно указаны в схеме
+}).unknown(true);
 
-// Валидируем переменные окружения
 const { error, value: envVars } = schema.validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
-// Определяем интерфейс для конфигурации
 interface Config {
   datasourceUrl: string;
   jwtSecret: string;
   appPort: number;
 }
 
-// Экспортируем валидированные переменные окружения
 const env: Config = {
   datasourceUrl: envVars.DATASOURCE_URL,
   jwtSecret: envVars.JWT_SECRET,
