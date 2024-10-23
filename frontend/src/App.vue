40409a44823c2197f15ea4b13f37748636f2fb1b
le-xot <lexot.dev@gmail.com>
2024-10-23 13:29:07 +0500

    fix: videos statuses

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import Body from './components/Body.vue';
-import { darkTheme, NConfigProvider } from 'naive-ui';
+import {
+  darkTheme,
+  NConfigProvider,
+  NLayout,
+  NLayoutContent,
+  NLayoutFooter,
+  NLayoutHeader,
+} from 'naive-ui';
+import Videos from './components/Videos.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
   <n-config-provider :theme="darkTheme">
-    <Body></Body>
+    <n-layout style="height: 100%">
+      <n-layout-header style="height: 5vh">
+        <Header />
+      </n-layout-header>
+      <n-layout-content style="height: 90vh">
+        <Videos />
+      </n-layout-content>
+      <n-layout-footer style="height: 5vh">Footer</n-layout-footer>
+    </n-layout>
   </n-config-provider>
 </template>
