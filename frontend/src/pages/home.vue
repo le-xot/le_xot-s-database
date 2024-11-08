c439af5d761bfc90375746aaa57101c9ac87424b
le-xot <lexot.dev@gmail.com>
2024-11-08 05:17:35 +0500

    chore:adaptive for mobile

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -11,10 +11,10 @@ interface Link {
 const links: Link[] = [
   { name: 'Telegram', icon: TelegramIcon, color: '#26A5E4', url: 'https://t.me/le_xot' },
   { name: 'Discord', icon: DiscordIcon, color: '#5865F2', url: 'https://discord.gg/zgZHHnkkP5' },
-  { name: 'YouTube', icon: YouTubeIcon, color: '#FF0000', url: 'https://www.youtube.com/@le_xot' },
   { name: 'Twitch', icon: TwitchIcon, color: '#9146FF', url: 'https://www.twitch.tv/le_xot' },
-  { name: 'GitHub', icon: GitHubIcon, color: '#181717', url: 'https://github.com/le-xot' },
+  { name: 'YouTube', icon: YouTubeIcon, color: '#FF0000', url: 'https://www.youtube.com/@le_xot' },
   { name: 'Steam', icon: SteamIcon, color: '#000000', url: 'https://steamcommunity.com/id/le_xot' },
+  { name: 'GitHub', icon: GitHubIcon, color: '#181717', url: 'https://github.com/le-xot' },
 ]
 </script>
 
@@ -38,7 +38,7 @@ const links: Link[] = [
             class="button__logo"
             size="32px"
           />
-          <span class="button__text">{{ link.name }}</span>
+          <span class="button_text">{{ link.name }}</span>
         </a>
       </div>
       <a class="button button--large">
@@ -122,8 +122,55 @@ const links: Link[] = [
     width: 100%;
   }
 
-  .button__text{
+  .button_text{
     font-size: 1rem;
     text-align: center;
+  }
+
+  @media (max-width: 600px) {
+    .container {
+      padding: 5rem;
+    }
+
+    .container__items {
+      gap: 0.5rem;
+      max-width: 100%;
+    }
+
+    .title {
+      font-size: 1.5rem;
+    }
+
+    .logo {
+      border-radius: 1rem;
+    }
+
+    .buttons {
+      grid-template-columns: 1fr;
+      gap: 0.5rem;
+    }
+
+    .button {
+      min-height: 52px;
+      color: white;
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      fill: #ffffff;
+      padding: .5rem 1rem;
+      background: #209852;
+      font-size: 1rem;
+      font-weight: 700;
+      letter-spacing: .025em;
+      text-align: center;
+      border-radius: .5rem;
+      border: 2px solid #ffffff3e;
+      transition: .3s ease-in-out;
+    }
+
+    .button_text {
+      font-size: 0.875rem;
+    }
   }
 </style>
