52d6e500611cb3aac48b1a86b5aca0abd83f4ad7
le-xot <lexot.dev@gmail.com>
2024-11-08 16:19:13 +0500

    feat(backend): add serve static

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,6 @@
+import { join } from 'node:path'
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { PrismaModule } from './database/prisma.module'
 import { AdminsModule } from './modules/admin/admin.module'
 import { AuthModule } from './modules/auth/auth.module'
@@ -9,6 +11,9 @@ import { VideoModule } from './modules/video/video.module'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'frontend', 'dist'),
+    }),
     AuthModule,
     AdminsModule,
     PersonModule,
