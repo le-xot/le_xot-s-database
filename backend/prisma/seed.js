b6a70ceb684ad0949d7af25519824a2d5486c2cb
le-xot <lexot.dev@gmail.com>
2024-11-11 16:01:00 +0500

    feat(backend): add migration from notion

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 require('dotenv').config()
+const fs = require('node:fs')
+const path = require('node:path')
 const process = require('node:process')
 const bcrypt = require('bcrypt')
 
@@ -18,6 +20,45 @@ async function seed() {
       },
     },
   )
+
+  const gamesPath = path.join(__dirname, 'game.json')
+  const moviesPath = path.join(__dirname, 'movie.json')
+
+  const games = JSON.parse(fs.readFileSync(gamesPath, 'utf8'))
+  const movies = JSON.parse(fs.readFileSync(moviesPath, 'utf8'))
+
+  for (const movie of movies) {
+    try {
+      await prisma.video.create({
+        data: {
+          title: movie.title,
+          status: movie.status,
+          genre: movie.genre,
+          grade: movie.grade,
+          person: { connectOrCreate: { where: { name: movie.person }, create: { name: movie.person } } },
+        },
+      })
+    } catch (e) {
+      console.log(movie)
+      throw e
+    }
+  }
+
+  for (const game of games) {
+    try {
+      await prisma.game.create({
+        data: {
+          title: game.title,
+          status: game.status,
+          grade: game.grade,
+          person: { connectOrCreate: { where: { name: game.person }, create: { name: game.person } } },
+        },
+      })
+    } catch (e) {
+      console.log(game)
+      throw e
+    }
+  }
 }
 
 seed()
